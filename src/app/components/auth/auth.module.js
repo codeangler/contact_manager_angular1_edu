cfa1a04789b7013e2ec94cc337ed6c9582e19de7
codeangler <dev@codeangler.com>
2016-11-30 14:33:15 -0700

    initial commit || firebase config set up

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,24 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+
+  .config(function($firebaseRefProvider) {
+    var config = {
+    apiKey: "AIzaSyDjbdFZxZe6-h8v_-i19m_09LCSJpaHJpw",
+    authDomain: "contact-manager-clb.firebaseapp.com",
+    databaseURL: "https://contact-manager-clb.firebaseio.com",
+    storageBucket: "contact-manager-clb.appspot.com",
+    messagingSenderId: "599612533187"
+    };
+    
+    $firebaseRefProvider
+      .registerUrl({
+        default: config.databaseURL,
+        default: config.databaseURL + '/contacts'
+      })
+
+    firebase.initializeApp(config);
+  })
+
+  ;
